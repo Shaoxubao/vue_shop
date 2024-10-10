9356dd16f39a85e4161ea2d356e1de4e0aac900a
shaoxubao <xubao_shao@163.com>
2024-10-10 10:23:18 +0800

    登录验证

--- src/components/LoginCom.vue
+++ src/components/LoginCom.vue
@@ -5,17 +5,19 @@
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="">
       </div>
-      <!-- 登录区域 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
-        <el-form-item>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
-        <el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
           <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
         </el-form-item>
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -28,9 +30,27 @@ export default {
     return {
       // 登录表单的数据绑定对象
       loginForm: {
-        username: 'zs',
-        password: '111'
+        username: '',
+        password: ''
+      },
+      // 表单验证规则对象
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    // 重置按钮，重置登录表单
+    resetLoginForm() {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields()
     }
   }
 }
