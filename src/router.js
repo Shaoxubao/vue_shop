864490c8fa77f9beb62e2e05e84a1c9dc9c099d1
shaoxubao <xubao_shao@163.com>
2024-10-25 18:08:07 +0800

    权限角色列表

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import LoginCom from './components/LoginCom.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
+import Rights from './components/power/Rights.vue'
+import Roles from './components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -11,7 +13,17 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: LoginCom },
-    { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }] }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles }
+      ]
+    }
   ]
 })
 
